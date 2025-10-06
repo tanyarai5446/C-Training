0058d6f2d8da577d2c44160793d2afb0ab2f08ab
tanyarai5446 <tr5335074@gmail.com>
2025-10-06 23:55:12 +0530

    string

--- c52.c
+++ c52.c
@@ -1,16 +1,12 @@
 
 #include <stdio.h>
 //spiral matrix
-
 int main()
 {
     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}
-
     };
-
     int t = 0, b = 2;
     int left = 0, right = 2;
-
     while (t <= b && left <= right)
     {
         for (int i = left; i <= right; i++)
@@ -18,19 +14,15 @@ int main()
             printf("%d ", arr[t][i]);
         }
         t++;
-
         for (int i = t; i <= b; i++)
             printf("%d ", arr[i][right]);
         right--;
-
         for (int i = right; i >= left; i--)
             printf("%d ", arr[b][i]);
         b--;
-
         for (int i = b; i >= t; i--)
             printf("%d ", arr[i][left]);
         left++;
     }
-
     return 0;
 }
