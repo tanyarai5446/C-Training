caf4d5c97b431f8bcebd4fc10efe5fb8940d45b5
tanyarai5446 <tr5335074@gmail.com>
2025-11-17 22:51:47 +0530

    max of 2numbers

--- c07.c
+++ c07.c
@@ -4,15 +4,14 @@ int main()
 {
     int i,j;
     scanf("%d%d",&i,&j);
-
     if(i>j)
-     printf("%d is greatest",i);
-    else{
+         printf("%d is greatest",i);
+    else
+    {
         if(i==j)
          printf("i and j are equal = %d",i);
         else 
          printf("%d is greatest",j);
-
     } 
     return 0;
 }
