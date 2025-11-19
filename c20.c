79dfd5d2a1bac96676bfedc372e02136609622c8
tanyarai5446 <tr5335074@gmail.com>
2025-11-19 22:11:14 +0530

    pyramid pointing right

--- c20.c
+++ c20.c
@@ -6,10 +6,11 @@ int main()
     int n;
     printf("enter your rows:");
     scanf("%d",&n);
-    for(int i=0;i<2*n-1;i++){
+    for(int i=0;i<2*n-1;i++)
+    {
         int comp = n - abs(n-1-i);
         for(int j=0;j<comp;j++)
-            printf("*");
+        printf("*");
         printf("\n");
     }
     return 0;
