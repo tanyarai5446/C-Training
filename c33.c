84135edcea4c771a59b11560c86ac669937029f4
tanyarai5446 <tr5335074@gmail.com>
2025-11-21 21:42:35 +0530

    sum of array elements

--- c33.c
+++ c33.c
@@ -5,10 +5,14 @@ int main()
     int a[6];
     int sum=0;
     printf("array elements");
-    for(int i=0;i<6;i++){
-     scanf("%d",&a[i]);}
-    for(int j=0;j<6;j++){
-     sum+=a[j];}
+    for(int i=0;i<6;i++)
+    {
+     scanf("%d",&a[i]);
+    }
+    for(int j=0;j<6;j++)
+    {
+     sum+=a[j];
+    }
     printf("Sum of array elements:%d",sum);
     return 0;
 }      
