a3a9e6229b32e378db7f86fdc58adccbfb77c81f
tanyarai5446 <tr5335074@gmail.com>
2025-11-22 23:25:14 +0530

    merging 2 arrays alternatively

--- c41.c
+++ c41.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 //merging two arrays alternatively
-int main(){
+int main()
+{
     int a[3]={1,2,3};
     int b[3]={4,5,6};
     int c[6];
@@ -9,8 +10,6 @@ int main(){
         
         c[j]=a[i];
         c[j+1]=b[i]; 
-        
         printf("%d %d ",c[j],c[j+1]);
     }
-
 }
