48c05abedea17fb06c2792d3999709cc366a5fab
tanyarai5446 <tr5335074@gmail.com>
2025-11-25 22:38:15 +0530

    pairs of sum k

--- c49.c
+++ c49.c
@@ -4,12 +4,14 @@ int main()
 {
     int a[7]={1,3,4,6,-5,5,1};
     int i,j,k=7;
-    for(i=0;i<7;i++){
+    for(i=0;i<7;i++)
+    {
         for(j=i-1;j<7;j++)
-    { 
+       { 
         if(a[i]+a[j]==k)
         {
             printf("\n(%d,%d) at a[%d],a[%d]",a[i],a[j],i,j);
         }
-    }   }
+       }   
+    }
 }
