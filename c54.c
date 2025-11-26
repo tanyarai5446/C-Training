4a5e8a4b1ba3ca440256b60606fea3f341d76b2c
tanyarai5446 <tr5335074@gmail.com>
2025-11-26 23:01:57 +0530

    bubble sort

--- c54.c
+++ c54.c
@@ -13,16 +13,17 @@ int main(){
         printf("%d ",a[i]);
     }
     //int min=a[0];
-    for(i=0;i<n-1;i++){
+    for(i=0;i<n-1;i++)
+    {
         for(int j=0;j<n-i-1;j++)
-        if(a[j]>a[j+1]){
+        if(a[j]>a[j+1])
+        {
             int temp=a[j];
             a[j]=a[j+1];
             a[j+1]=temp;
-            }
-
-        
-}    printf("\nArray in ascending order is\n");
+        }
+    }    
+    printf("\nArray in ascending order is\n");
     for(i=0;i<5;i++){
         printf("%d ",a[i]);
     }
