397b64f2162cffbf38d76edde585435f238bf13d
tanyarai5446 <tr5335074@gmail.com>
2025-11-27 19:27:29 +0530

    sum of 2d arrray elements

--- c63.c
+++ c63.c
@@ -2,7 +2,8 @@
 //sum of 2d array elements
 int main()
 {
-    int m,n;printf("Enter number of rows:");scanf("%d",&m);
+    int m,n;
+    printf("Enter number of rows:");scanf("%d",&m);
     printf("Enter number of coloumn:");scanf("%d",&n);
     int a[m][n];int sum=0;
     printf("Enter elements of array:");
@@ -10,14 +11,11 @@ int main()
      for(int j=0;j<n;j++)
       scanf("%d",&a[i][j]);
     for(int i=0;i<m;i++)
-    {
-     
-        
+    {  
      for(int j=0;j<n;j++)
      {
         sum+=a[i][j];
      }
-     
     } 
     printf("Sum of matrix elements=%d",sum);
     return 0;
