c21d9f872aee2f04235fd1336ef739c70a8707d6
tanyarai5446 <tr5335074@gmail.com>
2025-11-27 19:25:03 +0530

    array pointer

--- c60.c
+++ c60.c
@@ -7,10 +7,10 @@ int main()
     for(int i=0;i<n;i++)
      scanf("%d",&a[i]);
     p=a;
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
+    {
      printf("%d ",*p);
      p++;
     }  
     return 0;
-
 }
