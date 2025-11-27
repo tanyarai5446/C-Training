aec88574284e7d81e2d714bb262fcb9fb4412dc4
tanyarai5446 <tr5335074@gmail.com>
2025-11-27 19:28:10 +0530

    rowwise sum of 2d array

--- c64.c
+++ c64.c
@@ -2,7 +2,8 @@
 //rowwise sum of 2d array
 int main()
 {
-    int m,n;printf("Enter number of rows:");scanf("%d",&m);
+    int m,n;
+    printf("Enter number of rows:");scanf("%d",&m);
     printf("Enter number of coloumn:");scanf("%d",&n);
     int a[m][n];
     printf("Enter elements of array:");
@@ -12,7 +13,6 @@ int main()
     for(int i=0;i<m;i++)
     {
      int sum=0;
-        
      for(int j=0;j<n;j++)
      {
         sum+=a[i][j];
