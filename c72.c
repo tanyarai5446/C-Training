ff60cf1867bff29fb0c38b6a8b00d10712ee1bfb
tanyarai5446 <tr5335074@gmail.com>
2025-11-29 23:08:19 +0530

    string copy

--- c72.c
+++ c72.c
@@ -5,12 +5,12 @@ int main()
     char str1[100],str2[100];
     printf("enter the 1st string\n");
     fgets(str1,100,stdin);
-    
     int l=strlen(str1);
     printf("length is %d\n",l-1);
     
     printf("copied string is\n");
-    for(int i=0;str1[i]!='\0';i++){
+    for(int i=0;str1[i]!='\0';i++)
+    {
     str2[i]=str1[i];
     printf("%c",str2[i]);
     }
