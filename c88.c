cbcc1589cd328c10dfc8906f251357dfd31b27a2
tanyarai5446 <tr5335074@gmail.com>
2025-12-02 23:09:23 +0530

    pal check on removal of any char

--- c88.c
+++ c88.c
@@ -17,7 +17,8 @@ bool ValidPal(char *s){
     int left = 0 , right = strlen(s)-1;
     while (left<right)
     {
-         if(s[left]!=s[right]){
+         if(s[left]!=s[right])
+         {
             return isPalindrome(s,left+1,right) ||
             isPalindrome(s,left,right-1) ;
         }
@@ -30,10 +31,12 @@ int main(){
     char s[100];
     printf("Enter a string: ");
     scanf("%s",s);
-    if(ValidPal(s)){
+    if(ValidPal(s))
+    {
         printf("Yes it can be valid palindrom on one character removal");
     }
-    else{
+    else
+    {
         printf("aint valid palidrome even after removing one");
     }
 }
