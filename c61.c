0eb25e30b32fb39af72dd2f44eb058e148674991
tanyarai5446 <tr5335074@gmail.com>
2025-12-05 22:58:49 +0530

    check if 2 array equal

--- c61.c
+++ c61.c
@@ -2,8 +2,12 @@
 //check two arrays are equal or not using pointers
 int main()
 {
-     int n;printf("enter sizeof 1st array:");scanf("%d",&n);
-     int m;printf("enter sizeof 2nd array:");scanf("%d",&m);
+     int n;
+     printf("enter sizeof 1st array:");
+     scanf("%d",&n);
+     int m;
+     printf("enter sizeof 2nd array:");
+     scanf("%d",&m);
      int a[n],b[m],*p1=a,*p2=b,flag=0;
      if(sizeof(a)!=sizeof(b))
       printf("array not equal");
