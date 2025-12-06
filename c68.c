14ce8b1f535d4cf2b0ae1d939c2821fd9c5516e4
tanyarai5446 <tr5335074@gmail.com>
2025-12-06 23:53:05 +0530

    2d to 1d array conversion

--- c68.c
+++ c68.c
@@ -3,8 +3,10 @@
 int main()
 {
     int m,n;
-    printf("Enter number of rows:");scanf("%d",&m);
-    printf("Enter number of coloumn:");scanf("%d",&n);
+    printf("Enter number of rows:");
+    scanf("%d",&m);
+    printf("Enter number of coloumn:");
+    scanf("%d",&n);
     int a[m][n],b[m*n],count=0;
     printf("Enter elements of array:");
     for(int i=0;i<m;i++)
